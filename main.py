b38dbdea554289b55f4a926f6706e29198cccbd7
sletzt <haidarharimaugame18@gmail.com>
2025-05-18 17:26:05 +0700

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-main.py
+meme_dict = {
+            "CRINGE": "Sesuatu yang sangat aneh atau memalukan",
+            "LOL": "Tanggapan umum terhadap sesuatu yang lucu",
+            "ROFL": "tanggapan terhadap lelucon",
+            "SHEESH": "sedikit ketidaksetujuan",
+            "CREEPY": "menakutkan, tidak menyenangkan",
+            "AGGRO": "untuk menjadi agresif/marah"
+            }
+
+word = input("Ketik kata yang tidak Kamu mengerti (gunakan huruf kapital semua!): ")
+
+if word in meme_dict.keys():
+   print(meme_dict[word])
+else:
+    print("Kata yang anda cari tidak ada")
